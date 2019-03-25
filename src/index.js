f9a578f614932e8a0f8baa6de430970aeb6ab877
ruxiu.zheng <ruxiu.zheng@bkjk.com>
2019-03-25 10:31:52 +0800

    加载处理图片资源管理

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import _ from 'lodash'
 import './style.css'
+import Dog from './dog.jpg'
 function component() {
   let element = document.createElement('div')
   element.innerHTML = _.join(['hello', 'webpack'], ' ')
   element.classList.add('hello');
+  // 添加图片到 div
+  let myDog = new Image()
+  myDog.src = Dog;
+  element.appendChild(myDog)
   return element
 }
 document.body.appendChild(component())
