734a278e52453ceb6055364309e7f5140a12f843
ruxiu.zheng <ruxiu.zheng@bkjk.com>
2019-03-25 11:34:13 +0800

    用插件自动生成输出文件index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({title: 'OUTPUT MANAGEMENT'})
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
