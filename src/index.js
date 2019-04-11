855958488aeca74d6ceb8165a6ce5d6c053eea2b
ruxiu.zheng <ruxiu.zheng@bkjk.com>
2019-04-11 10:31:49 +0800

    使用自己成熟的npm包

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import _ from 'lodash'
 import numRef from './ref.json'
 import Aeolus from 'aeolus-package'
-console.log(Aeolus.DateTransform.toDate('20190409'))
+console.log(Aeolus.DateTransform.toDate(new Date()))
+console.log(Aeolus.DateTransform.toNoSpaceDate('2018-12-24'))
+console.log(Aeolus.StringTransform.initialsCapital('i am 25 years old'))
 export function numToWorld(num) {
   return _.reduce(numRef, (accum, ref) => {
     return ref.num === num ? ref.word : accum
