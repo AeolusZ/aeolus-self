ab51fa368e51c1910a95110c626aea476733086d
ruxiu.zheng <ruxiu.zheng@bkjk.com>
2019-04-12 17:49:03 +0800

    shimming

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,10 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    polyfills: './src/polyfills',
+    index: './src/index'
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
