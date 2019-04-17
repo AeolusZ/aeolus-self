31fa7714fd327ad42782efa07c132b666a83711f
ruxiu.zheng <ruxiu.zheng@bkjk.com>
2019-04-17 11:45:34 +0800

    shimming细粒度

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import Aeolus from 'aeolus-package'
-console.log(Aeolus.DateTransform.toDate(new Date()))
-console.log(Aeolus.DateTransform.toNoSpaceDate('2018-12-24'))
-console.log(Aeolus.StringTransform.initialsCapital('i am 25 years old'))
+const Aeolus = require('aeolus-package')
+const global = require('./globals.js')
+console.log(Aeolus.default)
+console.log(Aeolus.default.DateTransform.toDate(new Date()))
+console.log(Aeolus.default.DateTransform.toNoSpaceDate('2018-12-24'))
+console.log(Aeolus.default.StringTransform.initialsCapital('i am 25 years old'))
+console.log(global.file)
+console.log(global.parse)
 
 function component() {
   var element = document.createElement('div')
+  this.alert('Hmmm, this probably isn\'t a great idea...')
   element.innerHTML = join(['hello', 'webpack'], ' ');
   return element
 }
--- webpack.common.js
+++ webpack.common.js
@@ -3,13 +3,21 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 module.exports = {
-  entry: {
-    polyfills: './src/polyfills',
-    index: './src/index'
-  },
+  entry: './src/index.js',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {test: /\.css$/, use: ['style-loader', 'css-loader']},
+      {test: /\.(png|svg|jpg|gif)$/, use: ['file-loader']},
+      {test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader']},
+      {test: /\.(csv|tsv)$/, use: ['csv-loader']},
+      {test: /\.xml$/, use: ['xml-loader']},
+      {test: require.resolve('./src/index'),use: 'imports-loader?this=>window'},
+      {test: require.resolve('./src/globals'),use: 'exports-loader?file,parse=helpers.parse'}
+    ]
   },
   plugins: [
     new CleanWebpackPlugin(),
@@ -20,5 +28,5 @@ module.exports = {
       _: 'lodash',
       join: ['lodash', 'join']
     })
-  ],
+  ], 
 }
